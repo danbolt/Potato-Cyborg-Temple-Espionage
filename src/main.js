c86e8ef93819afd82ca7d9aafd986f4ee47264b4
Daniel Savage <dansvg@gmail.com>
2016-10-21 15:34:25 -0700

    Hello world with Phaser and bitmap text

---
+++ src/main.js
@@ -0,0 +1,41 @@
+var Preload = function () {
+	//
+};
+Preload.prototype.init = function() {
+  this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  this.game.scale.refresh();
+
+  this.game.scale.pageAlignHorizontally = true;
+  this.game.scale.pageAlignVertically = true;
+
+  // enable crisp rendering
+  this.game.stage.smoothed = false;
+  this.game.renderer.renderSession.roundPixels = true;  
+  Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+  PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST; //for WebGL
+
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.DOWN);
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.UP);
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
+
+  this.game.input.gamepad.start();
+};
+Preload.prototype.preload = function() {
+
+  // Font is Gamegirl Classic by freakyfonts
+  // License is for noncommercial use
+  // http://www.fontspace.com/freaky-fonts/gamegirl-classic
+  this.game.load.bitmapFont('font', 'asset/font/font.png', 'asset/font/font.json');
+};
+Preload.prototype.create = function() {
+ 	this.game.add.bitmapText(32, 32, 'font', 'hello, world!', 8);
+};
+
+var main = function () {
+	console.log('hello, jam! 😊');
+
+	var game = new Phaser.Game(320, 240);
+	game.state.add('Preload', Preload, false);
+
+	game.state.start('Preload');
+};
