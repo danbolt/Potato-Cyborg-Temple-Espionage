3ca4258c8ea47e9ffe98456b4bef9efe3f997d84
Daniel Savage <dansvg@gmail.com>
2016-10-23 21:01:31 -0700

    Update Win screen

--- src/win_screen.js
+++ src/win_screen.js
@@ -4,9 +4,13 @@ var WinScreen = function () {
 WinScreen.prototype.create = function() {
 	this.game.camera.reset();
 
-	this.game.add.bitmapText(32, 32, 'font', 'nice you win ^_^', 8);
+	var loseText = this.game.add.bitmapText(this.game.width / 2, this.game.height / 2, 'font', 'Well done Lisa!\nYou got all the lip balm and escaped!\n\nYou are a spy enough!\n\nPress Space to go back to title.', 8);
+	loseText.align = 'center';
+	loseText.anchor.set(0.5, 0);
 
-	this.game.time.events.add(2000, function () {
-		this.game.state.start('Gameplay');
+
+	this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR).onUp.add(function () {
+		SoundBank['select'].play();
+		this.game.state.start('TitleScreen');
 	}, this);
 };
